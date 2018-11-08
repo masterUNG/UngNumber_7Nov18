cc384af49ed646c6a643c119f01211d01e5890e6
masterUNG <phrombutr@gmail.com>
2018-11-08 11:57:26 +0700

    Get Value From TextField

--- UngNumber/RegisterViewController.swift
+++ UngNumber/RegisterViewController.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+//    Explicit
+    var nameString: String?
+    var userString: String?
+    var passwordString: String?
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+   
+    @IBAction func cloudUploadButton(_ sender: Any) {
+        
+//        Get Value From TextField
+        nameString = nameTextField.text
+        print("nameString ==> \(String(describing: nameString))")
+        // nameString ==> Optional
+        
+        let myNameString: String = nameString!
+        
+        userString = userTextField.text
+        let myUserString: String = userString!
+        
+        passwordString = passwordTextField.text
+        let myPasswordString: String = passwordString!
+        
+        print("myNameStrign ==> \(myNameString)")
+        print("myUserStrign ==> \(myUserString)")
+        print("myPasswordStrign ==> \(myPasswordString)")
+        
+        
+        
+    }   // cloudUpload
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
